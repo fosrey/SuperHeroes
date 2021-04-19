4634fb25ec47808d37ab5a2fe225375d5b0675ec
Fosrey <fosrey@gmail.com>
2021-04-19 14:27:09 -0500

    Boton de borrar y css

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -2,10 +2,17 @@ import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent{
+
   heroes: string[] = ['hulk','saitama','superman','goku'];
+
+
+  removeItem(i: number){
+    this.heroes.splice(i, 1);
+  }
 }
